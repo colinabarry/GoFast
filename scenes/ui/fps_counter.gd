015667e04475f641f91484814553f626a362b8e4
colinabarry <colin.andrew.barry@gmail.com>
2024-01-01 03:44:36 -0500

    average fps

--- scenes/ui/fps_counter.gd
+++ scenes/ui/fps_counter.gd
@@ -1,6 +1,13 @@
 extends Label
 
+var fps_sum := 0.0
+var average_fps := 0.0
+
 
 func _process(_delta: float) -> void:
 	var fps := Engine.get_frames_per_second()
-	text = str(fps)
+
+	fps_sum += fps
+	var frame_count := Engine.get_frames_drawn()
+	average_fps = fps_sum / frame_count
+	text = str(fps) + " (" + str(average_fps as int) + " avg)"
